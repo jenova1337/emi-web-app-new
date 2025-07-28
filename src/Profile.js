8c8bbbdf3f6b3883d096a38592c2a484ac008169
jenova1337 <your-email@example.com>
2025-07-28 14:43:52 +0530

    fix: edit profile continous typing

--- src/Profile.js
+++ src/Profile.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from "react";
-import { doc, getDoc, updateDoc } from "firebase/firestore";
+import React, { useEffect, useState, useCallback } from "react";
+import { auth, db } from "./firebase";
+import { doc, getDoc, setDoc } from "firebase/firestore";
 import { updatePassword } from "firebase/auth";
-import { db } from "./firebase";
-import useAuth from "./auth/useAuth";
+
 
 const Profile = () => {
   const { user } = useAuth();
